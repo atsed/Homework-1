80787396d89c72b44b96a5ac34cfcba7bc92e440
desta-study <masterenko1to@mail.ru>
2017-09-27 15:56:14 +0300

    Create main.cpp

---
+++ sources/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "stack.hpp"
+using namespace std;
+
+int main() 
+{
+	cout << "ЭЛЕМЕНТЫ СТЕКА: " << endl;
+		
+	stack<int> intStack;
+	intStack.push(0);
+	intStack.push(1);
+	intStack.push(2);
+	intStack.push(3);
+	intStack.push(4);
+
+	for (int i = 0; i < 4; i++) 
+	{
+		cout << intStack.pop() << endl;
+	}
+		 
+	cout << "Номер элемента стека: " << intStack.count() << endl;
+	return 0;	
+}
