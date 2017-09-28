379de31d3079114c6fb0cdfda7ae9cc37224b24d
desta-study <masterenko1to@mail.ru>
2017-09-28 17:54:04 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,5 +1,5 @@
-#ifndef Stack_h
-#define Stack_h
+#ifndef STACK_HPP
+#define STACK_HPP
 
 #include <iostream>
 #include <cassert>
@@ -7,49 +7,46 @@
 using namespace std;
 
 template <typename T>
-class stack
-{
+class stack{
 private:
-	T * array_;
-	size_t array_size_;
-	size_t count_;
+    T * array_;
+    size_t array_size_;
+    size_t count_;
+	
 public:
-	stack() {
-		array_size_ = 10;
-		array_ = new T[array_size_];
-		count_ = 1;
-	}
-
-	stack(size_t n) {
-		array_size_ = n;
-		array_ = new T[array_size_];
-		count_ = 1;
-	}
-
-	size_t count() const {
-		return count_;
-	}
-
-	void push(T const &value) {
-		if (count_ == array_size_) {
-			T *array_2 = new T[2 * array_size_];
-			for (size_t i = 0; i != array_size_; i++)
-				array_2[i] = array_[i];
-			delete[] array_;
-			array_ = array_2;
-		}
-		array_[count_++] = value;
-	}
-
-	T pop() {
-		assert(count_ != 0);
-		return array_[--count_];
-	}
-
-	void print_stack() {
-		for (size_t i = count_ - 1; i > 0; i--)
-			cout << array_[i] << endl;
-	}
+    stack(){
+        array_size_ = 10;
+        array_ = new T[array_size_];
+        count_ = 1;
+    }
+    
+    size_t count() const{             //находим количество элементов в стеке
+        return count_;
+    }
+    
+    void push(T const &value){             //перераспределение памяти
+        if (count_ == array_size_){
+            T *array_2 = new T[2 * array_size_];
+            
+            for (size_t i = 0; i != array_size_; i++)
+                array_2[i] = array_[i];
+            
+            delete[] array_;
+            array_ = array_2;
+        }
+        
+        array_[count_++] = value;
+    }
+    
+    T pop(){                             //удалить элемент 
+        assert(count_ != 0);
+        return array_[--count_];
+    }
+    
+    void print(){                         //печатаем элементы стека
+        for (size_t i=count_-1; i>0; i--)
+            cout << array_[i] << endl;
+    }
 };
 
 #endif
