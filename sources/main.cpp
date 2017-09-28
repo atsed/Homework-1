f7cd01aa9e6614ec51ca2e38b453544c31c845d5
desta-study <masterenko1to@mail.ru>
2017-09-28 17:58:52 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include "stack.hpp"
+
 using namespace std;
 
-int main() 
-{
-	cout << "ЭЛЕМЕНТЫ СТЕКА: " << endl;
-		
-	stack<int> intStack;
-	intStack.push(0);
-	intStack.push(1);
-	intStack.push(2);
-	intStack.push(3);
-	intStack.push(4);
-
-	for (int i = 0; i < 4; i++) 
-	{
-		cout << intStack.pop() << endl;
-	}
-		 
-	cout << "Номер элемента стека: " << intStack.count() << endl;
-	return 0;	
+int main(){
+    
+    stack<int> intStack;	   //создание переменной класса stack
+    
+    intStack.push(0);		   //добавление элемента в стек 	
+    intStack.push(1);
+    intStack.push(2);
+    intStack.push(3);
+    intStack.push(4);
+    intStack.push(5);
+    intStack.push(6);
+    intStack.push(7);
+    intStack.push(8);
+    intStack.push(9);
+    
+    cout << "ВЫВОД СТЕКА В КОНСОЛЬ: " << endl;
+    intStack.print();
+    
+    for (int i = 0; i < 10; i++) {		//удаление элемента
+        cout << intStack.pop() << endl;			
+    }    
+    return 0;
 }
